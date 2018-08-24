07c56b68244eb1f03dbcd671d632b84ad4694db9
Dzimid <jac3k.dziemidowicz@gmail.com>
2018-08-24 09:50:57 +0200

    documentation v3

--- src/main/java/pl/ncdc/hot3/pooltable/PoolTable/services/CueService.java
+++ src/main/java/pl/ncdc/hot3/pooltable/PoolTable/services/CueService.java
@@ -245,6 +245,14 @@ public class CueService {
         return ((min1 + min2)/2);
     }
 
+    /**
+     * Calculate distance between two points
+     *
+     * @param point1 point
+     * @param point2 point
+     *
+     * @return distance
+     */
     public static double getDistanceBetweenPoints(Point point1, Point point2) {
         return Math.sqrt(Math.pow((point2.x - point1.x), 2) + Math.pow((point2.y - point1.y), 2));
     }
@@ -292,6 +300,17 @@ public class CueService {
                 Math.sqrt(Math.pow(coordinates[0], 2) + Math.pow(coordinates[1], 2));
     }
 
+    /**
+     * Magic method which calculate two points on aiming line based on distance from ball center. Ghost ball definition
+     * - http://www.easypooltutor.com/img/lessons/ghost_ball.png
+     *
+     * @param line aiming line
+     * @param ball collision ball
+     *
+     * @return collision line based on object ball and ghost ball
+     *
+     * @throws LineServiceException if can not get extended cue line for one side
+     */
     public Line findBallCollisionLine(Line line, Ball ball) throws LineServiceException {
         double[] coordinates = calcAllCoordinate(line);
         double A = coordinates[0];
